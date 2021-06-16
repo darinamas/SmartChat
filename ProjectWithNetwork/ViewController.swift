167408e5064781b59402ee959dff29ffe74933af
Darina <daryna.massive@gmail.com>
2021-06-16 12:13:24 +0200

    Initial Commit

---
+++ ProjectWithNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectWithNetwork
+//
+//  Created by Daryna Polevyk on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
